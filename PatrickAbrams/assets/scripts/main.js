3d296bc7dc8f0894708bccb6be1820684c20e172
PatrickAbrams <patrickabramsmail@gmail.com>
2019-10-08 23:25:18 -0400

    HTML Markup for Roleplaying Game

---
+++ PatrickAbrams/assets/scripts/main.js
@@ -0,0 +1,11 @@
+/*for( var count=0; count < 10; ++count){
+    
+    var timeBetween = setInterval(newBlock,1000);
+    if (count = 10) break;
+    function newBlock(){
+        var target = document.createElement("div");
+        target.setAttribute("class","target-style");
+        document.body.appendChild(target);
+    }
+}
+*/
