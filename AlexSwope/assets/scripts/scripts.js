ae635d7ffb05323d48c62487a289cba2ebb044fe
alexswope <awswope@gmail.com>
2019-10-14 09:36:07 -0400

    click coordinates tracking

--- AlexSwope/assets/scripts/scripts.js
+++ AlexSwope/assets/scripts/scripts.js
@@ -4,7 +4,25 @@ console.log("Hello, Alex");
 function draw(){
     var canvas = document.getElementById('mouseTrainer');
     var ctx = canvas.getContext('2d');
+
+    ctx.fillStyle= 'rgb(219,193,198)';
+    ctx.fillRect(10,10,50,50)
+
+
+    ctx.fillStyle = 'rgba(0,102,0,1)';
+    ctx.fillRect(30,30,50,50);
+    
+    
+    
 };
+
+function myClick(event){
+    var x = event.offsetX;
+    var y = event.offsetY;
+    console.log("x: "+x + ", y: " + y);
+}
+
+
 
 document.onload = draw();
 
